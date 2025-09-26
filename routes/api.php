fa327c64219df6126648629732ac15d24093fc13
Jorge <jebauza1989@gmail.com>
2025-09-26 16:39:20 +0000

    ready auth and api docs

--- routes/api.php
+++ routes/api.php
@@ -3,16 +3,16 @@
 use Illuminate\Support\Facades\Route;
 use App\Api\Auth\Controllers\AuthController;
 
-Route::group([
-    'middleware' => 'api',
-    'prefix' => 'auth'
-], function ($router) {
-    Route::post('/register', [AuthController::class, 'register'])->name('auth.register');
-    Route::post('/login', [AuthController::class, 'login'])->name('auth.login');
+Route::middleware('api')->group(function () {
 
-    Route::middleware('auth:api')->group(function () {
-        Route::get('/me', [AuthController::class, 'me'])->name('auth.me');
-        Route::get('/refresh', [AuthController::class, 'refresh'])->name('auth.refresh');
-        Route::post('/logout', [AuthController::class, 'logout'])->name('auth.logout');
+    Route::prefix('auth')->group(function () {
+        Route::post('/register', [AuthController::class, 'register'])->name('auth.register');
+        Route::post('/login', [AuthController::class, 'login'])->name('auth.login');
+
+        Route::middleware('auth:api')->group(function () {
+            Route::get('/me', [AuthController::class, 'me'])->name('auth.me');
+            Route::get('/refresh', [AuthController::class, 'refresh'])->name('auth.refresh');
+            Route::post('/logout', [AuthController::class, 'logout'])->name('auth.logout');
+        });
     });
 });
