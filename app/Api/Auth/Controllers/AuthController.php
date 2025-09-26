876a0065fab4e5417d382842d7b3463cab72688e
Jorge <jebauza1989@gmail.com>
2025-09-26 16:33:01 +0000

    add request-docs api

--- app/Api/Auth/Controllers/AuthController.php
+++ app/Api/Auth/Controllers/AuthController.php
@@ -2,7 +2,6 @@
 
 namespace App\Api\Auth\Controllers;
 
-use Validator;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
@@ -18,6 +17,28 @@ class AuthController extends Controller
      * Register a User.
      *
      * @return \Illuminate\Http\JsonResponse
+     */
+    /**
+     * @lrd:start
+     *
+     * **201 Created**
+     * ```json
+     *{"id":"9ff8ac68-6f9b-4c14-96a8-c4086f30fabf","email":"pepe@gmail.com","name":"Pepe Gonzalez"}
+     * ```
+     *
+     * **422 Unprocessable Entity**
+     * ```json
+     *{"name":["The name field is required."],"password":["The password field is required."]}
+     * ```
+     *
+     * **500 Internal Server Error**
+     * ```json
+     *{"message":"Internal Server Error"}
+     * ```
+     *
+     * @lrd:end
+     *
+     * @LRDresponses 201|422|500
      */
     public function register(RegisterRequest $request)
     {
@@ -43,7 +64,36 @@ class AuthController extends Controller
     /**
      * Get a JWT via given credentials.
      *
+     * @param LoginRequest $request [explicite description]
+     *
      * @return \Illuminate\Http\JsonResponse
+     */
+    /**
+     * @lrd:start
+     *
+     * **200 OK**
+     * ```json
+     *{"access_token": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOi8vbG9jYWxob3N0OjgwODAvYXBpL2F1dGgvbG9naW4iLCJpYXQiOjE3NTg4OTk4MDksImV4cCI6MTc1ODkwMzQwOSwibmJmIjoxNzU4ODk5ODA5LCJqdGkiOiJTc2s5VDc5Q2QxOHFxWThRIiwic3ViIjoiOWZmODU5MGUtOGU5My00NWY3LWFmZmQtZTc2NDQ1YzM2MDU1IiwicHJ2IjoiMjNiZDVjODk0OWY2MDBhZGIzOWU3MDFjNDAwODcyZGI3YTU5NzZmNyJ9.ObqGjR67L5bdkwbO9D5Vuth8mfoTiLbomIbWAYwvnSA", "token_type": "bearer", "expires_at": "2025-09-26 16:16:49"}
+     * ```
+     *
+     * **401 Unauthorized**
+     * ```json
+     *{"message":"Unauthenticated."}
+     * ```
+     *
+     * **422 Unprocessable Entity**
+     * ```json
+     *{"message":"The email field must be a valid email address.","errors":{"email":["The email field must be a valid email address."]}}
+     * ```
+     *
+     * **500 Internal Server Error**
+     * ```json
+     *{"message":"Internal Server Error"}
+     * ```
+     *
+     * @lrd:end
+     *
+     * @LRDresponses 200|401|422|500
      */
     public function login(LoginRequest $request)
     {
@@ -70,22 +120,38 @@ class AuthController extends Controller
      *
      * @return \Illuminate\Http\JsonResponse
      */
-    public function me()
+    /**
+     *
+     * @lrd:start
+     *
+     * **Set Global Headers**
+     * ```json
+     *{"Authorization": "Bearer <access_token>", "Content-Type": "application/json", "Accept": "application/json"}
+     * ```
+     *
+     * **200 OK**
+     * ```json
+     *{"id":"9ff8ac68-6f9b-4c14-96a8-c4086f30fabf","email":"pepe@gmail.com","name":"Pepe Gonzalez"}
+     * ```
+     *
+     * **401 Unauthorized**
+     * ```json
+     *{"message":"Unauthenticated."}
+     * ```
+     *
+     * **500 Internal Server Error**
+     * ```json
+     *{"message":"Internal Server Error"}
+     * ```
+     *
+     * @lrd:end
+     *
+     * @LRDresponses 200|401|500
+     */
+    public function me(Request $request)
     {
         $data = auth()->user()->only(User::ID, User::EMAIL, User::NAME);
         return response()->json($data);
-    }
-
-    /**
-     * Log the user out (Invalidate the token).
-     *
-     * @return \Illuminate\Http\JsonResponse
-     */
-    public function logout()
-    {
-        auth()->logout();
-
-        return response()->json(['message' => 'Successfully logged out']);
     }
 
     /**
@@ -93,9 +159,72 @@ class AuthController extends Controller
      *
      * @return \Illuminate\Http\JsonResponse
      */
-    public function refresh()
+    /**
+     *
+     * @lrd:start
+     *
+     * **Set Global Headers**
+     * ```json
+     *{"Authorization": "Bearer <access_token>", "Content-Type": "application/json", "Accept": "application/json"}
+     * ```
+     *
+     * **200 OK**
+     * ```json
+     *{"access_token": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOi8vbG9jYWxob3N0OjgwODAvYXBpL2F1dGgvbG9naW4iLCJpYXQiOjE3NTg4OTk4MDksImV4cCI6MTc1ODkwMzQwOSwibmJmIjoxNzU4ODk5ODA5LCJqdGkiOiJTc2s5VDc5Q2QxOHFxWThRIiwic3ViIjoiOWZmODU5MGUtOGU5My00NWY3LWFmZmQtZTc2NDQ1YzM2MDU1IiwicHJ2IjoiMjNiZDVjODk0OWY2MDBhZGIzOWU3MDFjNDAwODcyZGI3YTU5NzZmNyJ9.ObqGjR67L5bdkwbO9D5Vuth8mfoTiLbomIbWAYwvnSA", "token_type": "bearer", "expires_at": "2025-09-26 16:16:49"}
+     * ```
+     *
+     * **401 Unauthorized**
+     * ```json
+     *{"message":"Unauthenticated."}
+     * ```
+     *
+     * **500 Internal Server Error**
+     * ```json
+     *{"message":"Internal Server Error"}
+     * ```
+     *
+     * @lrd:end
+     *
+     * @LRDresponses 200|401|500
+     */
+    public function refresh(Request $request)
     {
         return $this->respondWithToken(auth()->refresh());
+    }
+
+    /**
+     * Log the user out (Invalidate the token).
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    /**
+     *
+     * @lrd:start
+     *
+     * **200 OK**
+     * ```json
+     *{"message":"Successfully logged out"}
+     * ```
+     *
+     * **401 Unauthorized**
+     * ```json
+     *{"message":"Unauthenticated."}
+     * ```
+     *
+     * **500 Internal Server Error**
+     * ```json
+     *{"message":"Internal Server Error"}
+     * ```
+     *
+     * @lrd:end
+     *
+     * @LRDresponses 200|401|500
+     */
+    public function logout(Request $request)
+    {
+        auth()->logout();
+
+        return response()->json(['message' => 'Successfully logged out']);
     }
 
     /**
@@ -107,10 +236,12 @@ class AuthController extends Controller
      */
     protected function respondWithToken($token)
     {
+        $expires_in_minutes = auth()->factory()->getTTL();
+
         return response()->json([
             'access_token' => $token,
             'token_type' => 'bearer',
-            'expires_in' => auth()->factory()->getTTL() * 60
+            'expires_at' => now()->addMinutes($expires_in_minutes)->toDateTimeString(),
         ]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -7,12 +7,12 @@ Route::group([
     'middleware' => 'api',
     'prefix' => 'auth'
 ], function ($router) {
-    Route::post('/register', [AuthController::class, 'register'])->name('register');
-    Route::post('/login', [AuthController::class, 'login'])->name('login');
+    Route::post('/register', [AuthController::class, 'register'])->name('auth.register');
+    Route::post('/login', [AuthController::class, 'login'])->name('auth.login');
 
     Route::middleware('auth:api')->group(function () {
-        Route::get('/me', [AuthController::class, 'me'])->name('me');
-        Route::get('/refresh', [AuthController::class, 'refresh'])->name('refresh');
-        Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+        Route::get('/me', [AuthController::class, 'me'])->name('auth.me');
+        Route::get('/refresh', [AuthController::class, 'refresh'])->name('auth.refresh');
+        Route::post('/logout', [AuthController::class, 'logout'])->name('auth.logout');
     });
 });
